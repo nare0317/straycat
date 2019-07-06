2d56668c48cb27ed84ec338b1349218730fcccae
nare0317 <nare317@gmail.com>
2019-07-06 17:10:30 +0900

    Missing_Read.jsp 수정 missing_read.js 파일 추가 missing_read.css 파일명 수정 & 파일위치변경

---
+++ WebContent/js/view/missing_read.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('#comment_input').keyup(function (e){
+        var content = $(this).val();
+        $(this).height(((content.split('\n').length + 1) * 1.5) + 'em');
+        $('#current-word').html(content.length);
+    });
+    $('#comment_input').keyup();
+});
