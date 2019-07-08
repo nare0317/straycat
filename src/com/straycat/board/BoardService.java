8013eaf7534b7f16691fc772998ddd888e6bfd44
김경환 <baer2000@naver.com>
2019-07-08 09:17:57 +0900

    mybatis 작업 진행 중

---
+++ src/com/straycat/board/BoardService.java
@@ -0,0 +1,10 @@
+package com.straycat.board;
+
+import java.util.List;
+
+import org.springframework.ui.Model;
+
+public interface BoardService 
+{
+	public List<BoardDTO> listBoard(Map<String, Object> map);
+}
