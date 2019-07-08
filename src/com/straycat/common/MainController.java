ce1e9ea2e1d4404d40e3583441860a4a99200ad7
nare0317 <nare317@gmail.com>
2019-07-08 10:31:08 +0900

    MainController.java 에서 한줄 삭제 (import org.springframework.web.context.support.StandardServletEnvironment;)

--- src/com/straycat/common/MainController.java
+++ src/com/straycat/common/MainController.java
@@ -3,7 +3,6 @@ package com.straycat.common;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.support.StandardServletEnvironment;
 
 @Controller
 public class MainController
