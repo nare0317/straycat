84c3594b41093b754216bb8a49c0615e2f583b1d
김경환 <baer2000@naver.com>
2019-07-08 10:19:59 +0900

    [오류 수정] mvc 패턴 복구

--- src/com/straycat/board/BoardService.java
+++ src/com/straycat/board/BoardService.java
@@ -6,5 +6,5 @@ import org.springframework.ui.Model;
 
 public interface BoardService 
 {
-	public List<BoardDTO> listBoard(Map<String, Object> map);
+	/* public List<BoardDTO> listBoard(Map<String, Object> map); */
 }
--- src/com/straycat/board/BoardServiceImpl.java
+++ src/com/straycat/board/BoardServiceImpl.java
@@ -1,31 +1,22 @@
-package com.straycat.board;
-
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service("board")
-public class BoardServiceImpl implements BoardService 
-{
-	@Autowired
-	private BoardDAO dao;
-	
-	@Override
-	public List<BoardDTO> listBoard(Map<String, Object> map) 
-	{
-		List<BoardDTO> list = null;
-		
-		try
-		{
-			list = dao.selectList("bbs.listBoard", map);
-		} catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-		
-		return list;
-	}
-	
-}
+/*
+ * package com.straycat.board;
+ * 
+ * import java.util.List; import java.util.Map;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.stereotype.Service;
+ * 
+ * @Service("board") public class BoardServiceImpl implements BoardService {
+ * 
+ * @Autowired private BoardDAO dao;
+ * 
+ * @Override public List<BoardDTO> listBoard(Map<String, Object> map) {
+ * List<BoardDTO> list = null;
+ * 
+ * try { list = dao.selectList("bbs.listBoard", map); } catch (Exception e) {
+ * e.printStackTrace(); }
+ * 
+ * return list; }
+ * 
+ * }
+ */
