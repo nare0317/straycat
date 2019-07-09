6fff03456ea80acd2feb2b676e02670dcf534e51
JaruKim <37865613+JaruKim@users.noreply.github.com>
2019-07-09 22:23:43 +0900

    입양 리스트로 이동하는 RequestMapping 삭제

--- src/com/straycat/common/MainController.java
+++ src/com/straycat/common/MainController.java
@@ -35,15 +35,6 @@ public class MainController
 		return "StrayCatManage";
 	}
 	
-	/*
-	// 입양 리스트 페이지로 이동
-	@RequestMapping("/adopt")
-	public String adoptList()
-	{
-		return "AdoptCatList";
-	}
-	*/
-	
 	// 실종 리스트 페이지로 이동
 	@RequestMapping("/missing")
 	public String missing()
