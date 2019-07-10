ccaf259ad0c77aff6b80965b24cd99788cdf88f9
mirror-neuron <neuronshork@gmail.com>
2019-07-10 19:56:35 +0900

    신청서 JS기능 수정중(textarea 크기 고정, 필수입력항목 cf.adopt_write)

--- WebContent/js/view/adoption_apply.js
+++ WebContent/js/view/adoption_apply.js
@@ -1,8 +1,26 @@
 $(function() {
+	
+	  // 서술형 질문1 300자 제한
       $('#textarea').keyup(function (e){
           var content = $(this).val();
           $(this).height(((content.split('\n').length + 1) * 1.5) + 'em');
           $('#counter').html(content.length + '/300');
       });
       $('#textarea').keyup();
+      
+      // 서술형 질문2 300자 제한
+      $('#textarea1').keyup(function (e){
+          var content = $(this).val();
+          $(this).height(((content.split('\n').length + 1) * 1.5) + 'em');
+          $('#counter1').html(content.length + '/300');
+      });
+      $('#textarea1').keyup();
+      
+      // 서술형 질문3 300자 제한
+      $('#textarea2').keyup(function (e){
+          var content = $(this).val();
+          $(this).height(((content.split('\n').length + 1) * 1.5) + 'em');
+          $('#counter2').html(content.length + '/300');
+      });
+      $('#textarea2').keyup();
 });
