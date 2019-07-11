8f172b4500aaebd905322021b167e52642289db2
JaruKim <baer2000@naver.com>
2019-07-12 00:19:54 +0900

    Revert "충돌 수정"
    This reverts commit d233b15543df36fc8af2f24617bb1007db01e888.

--- src/com/straycat/common/dao/LoginDAO.java
+++
@@ -1,8 +0,0 @@
-package com.straycat.common.dao;
-
-import java.util.Map;
-
-public interface LoginDAO
-{
-	public Map<String, Object> selectOne(String id, Object value);
-}
--- src/com/straycat/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.straycat.service;
-
-import java.util.Map;
-
-public interface LoginService
-{
-	public Map<String, Object> loginCheck(Map<String, Object> map);
-}
