9f7f577638c443c10b69f12033e540235c918963
JaruKim <baer2000@naver.com>
2019-07-16 00:08:11 +0900

    [기능 수정] 아이디나 비밀번호에 엔터를 입력하면 로그인을 수행하도록 기능 수정

--- WebContent/js/view/login.js
+++ WebContent/js/view/login.js
@@ -1,5 +1,6 @@
 $(document).ready(function()
 {
+	// 로그인 버튼을 클릭
 	$("#loginBtn").click(function()
 	{
 		// 아이디나 비밀번호가 입력되었는지 확인
@@ -9,37 +10,51 @@ $(document).ready(function()
 			return;
 		};
 		
-/*		$("#loginForm").submit();
-*/	});
+		loginAjax();
+	});
 	
-	$(document).ready(function()
+	// 아이디 입력란에서 엔터를 입력 시 로그인 시도
+	$("#inputId").keydown(function(key) {
+		if (key.keyCode == 13)
+		{
+			loginAjax();
+		}
+	})
+	
+	// 비밀번호 입력란에서 엔터를 입력 시 로그인 시도
+	$("#inputPwd").keydown(function(key) {
+		if (key.keyCode == 13)
+		{
+			loginAjax();
+		}
+	})
+	
+	// 로그인을 수행하는 Ajax
+	function loginAjax()
+	{
+		//console.log($("#inputId").val() , $("#inputPwd").val());
+		$.ajax({
+			type : "POST",
+			url : "login_check_ajax",
+			data : {'user_id' : $("#inputId").val() , 'user_pwd' : $("#inputPwd").val()},
+			success : function(data)
 			{
-				$("#loginBtn").click(function()
+				console.log(data);
+				if(data==1)
 				{
-					//console.log($("#inputId").val() , $("#inputPwd").val());
-					$.ajax({
-						type : "POST",
-						url : "login_check_ajax",
-						data : {'user_id' : $("#inputId").val() , 'user_pwd' : $("#inputPwd").val()},
-						success : function(data)
-						{
-							console.log(data);
-							if(data==1)
-							{
-								location.href='main';
-							}
-							else if(data==2)
-							{
-								alert("탈퇴한 회원의 정보입니다.");
-								return;
-							}
-							else
-							{
-								alert("아이디 또는 패스워드가 일치하지 않습니다");
-								return;
-							}
-						}					
-					});
-				});
-			});
+					location.href='main';
+				}
+				else if(data==2)
+				{
+					alert("탈퇴한 회원의 정보입니다.");
+					return;
+				}
+				else
+				{
+					alert("아이디 또는 패스워드가 일치하지 않습니다");
+					return;
+				}
+			}					
+		});
+	}
 });
