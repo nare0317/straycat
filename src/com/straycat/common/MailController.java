c474abad638950c7521110f46dbf61d25b49f8a3
김경환 <baer2000@naver.com>
2019-07-18 16:03:20 +0900

    [중간 커밋] 메일 구현 도중 취소됨

--- src/com/straycat/common/MailController.java
+++ src/com/straycat/common/MailController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,44 +31,52 @@ public class MailController
 	private ImageService imageService;
 
 	@RequestMapping(value="/mailsending", method=RequestMethod.POST)
-	public void mailSending(HttpServletRequest request, MultipartFile file, HttpSession session)
+	public String mailSending(HttpServletRequest request, MultipartFile file, HttpSession session)
 	{
 		String sender = request.getParameter("mailId")+"@"+request.getParameter("mailHost");
 		String receiver = "baer2000@naver.com";
 		String title = request.getParameter("title");
 		String content = request.getParameter("content");
-		
-		// 이미지를 저장하고 저장된 이미지 경로를 반환함
-		// 이미지 경로를 자료구조(고양이 등록정보)에 넣음
-		String path = session.getServletContext().getRealPath("/");
-		String imageUrl = imageService.saveImage(file, path);
-		
-		try 
+		try
 		{
-			MimeMessage message = mailSender.createMimeMessage();
-			MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
-			
-			messageHelper.setFrom(sender);
-			messageHelper.setTo(receiver);
-			messageHelper.setSubject(title);
-			messageHelper.setText(content);
-			
-			MimeBodyPart mbp = new MimeBodyPart();
-		    FileDataSource fds = new FileDataSource(imageUrl);
-		    mbp.setDataHandler(new DataHandler(fds));
-		    mbp.setFileName(fds.getName());
-			
-			Multipart mp = new MimeMultipart();
-			mp.addBodyPart(mbp);
-			
-			message.setContent(mp);
-			
-			mailSender.send(message);
-		} 
-		catch (MessagingException e) 
+		//MimeMessage message = mailSender.createMimeMessage();
+		MimeMessagePreparator preparator  = new MimeMessagePreparator()
 		{
-			System.out.println(e.toString());
+			
+			@Override
+			public void prepare(MimeMessage message) throws Exception
+			{
+				MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+				
+				messageHelper.setFrom(sender);
+				messageHelper.setTo(receiver);
+				messageHelper.setSubject(title);
+				messageHelper.setText(content);
+			}
+		};
+		
+		mailSender.send(preparator);
+		
 		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		/*
+		MimeBodyPart mbp = new MimeBodyPart();
+		FileDataSource fds = new FileDataSource(imageUrl);
+		mbp.setDataHandler(new DataHandler(fds));
+		mbp.setFileName(fds.getName());
+		
+		Multipart mp = new MimeMultipart();
+		mp.addBodyPart(mbp);
+		
+		message.setContent(mp);
+		*/
+		
+		
+		return "Main";
 	}
 	
 }
