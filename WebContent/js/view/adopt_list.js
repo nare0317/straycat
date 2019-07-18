0a7df839b6cfe8d1f385faa6b795ab82b77137cd
nare0317 <nare317@gmail.com>
2019-07-18 19:58:28 +0900

    학원작업내용2

--- WebContent/js/view/adopt_list.js
+++ WebContent/js/view/adopt_list.js
@@ -31,6 +31,55 @@ $(document).ready(function() {
 		
 	}).scroll();
 	
+	
+	
+	
+	//---------------------- 고양이사진 크기조절  -------------------------------- 
+	 /*$('.img-thumbnail img').each(function() 
+	 {*/
+	   //alert("함수호출");
+       var maxWidth = 200; // Max width for the image
+       var maxHeight = 200;    // Max height for the image
+       var ratio = 0;  // Used for aspect ratio
+       var width = $(".img_resize").width();    // Current image width
+       var height = $(".img_resize").height();  // Current image height
+
+       // Check if the current width is larger than the max
+       if(width > maxWidth){
+           ratio = maxWidth / width;   // get ratio for scaling image
+           $(".img_resize").css("width", maxWidth); // Set new width
+           $(".img_resize").css("height", height * ratio);  // Scale height based on ratio
+           height = height * ratio;    // Reset height to match scaled image
+           width = width * ratio;    // Reset width to match scaled image
+       }
+
+       // Check if current height is larger than max
+       if(height > maxHeight){
+           ratio = maxHeight / height; // get ratio for scaling image
+           $(".img_resize").css("height", maxHeight);   // Set new height
+           $(".img_resize").css("width", width * ratio);    // Scale width based on ratio
+           width = width * ratio;    // Reset width to match scaled image
+       }
+  /* });*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 });
 
 // 입양등록 버튼 클릭 시, 
